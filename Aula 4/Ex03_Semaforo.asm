c3b8009256d8476c1d47717c05b4cc501f0ea17d
LEONARDO SOUZA DE CASTRO <uniflcastro@feinet.fei.edu.br>
2025-03-10 20:06:07 -0300

    semaforo finalizado

--- Aula 4/Ex03_Semaforo.asm
+++ Aula 4/Ex03_Semaforo.asm
@@ -4,33 +4,33 @@
 	ORG 0100H
 INICIO:
 
-;Vermelho 
-	MOV P1, #00100100B ;Apartir do P1 e de cada bit dele vamos controlar nosso semáforo
 	JNB P1.3, CONTINUA ; Se o botão não estiver pressionado, continua normalmente
-
+;Vermelho 
+	
 ;Validar btn pressionado	
 	ACALL BTN_PRESS
 	CONTINUA:
+	MOV P1, #00100100B ; Apartir do P1 e de cada bit dele vamos controlar nosso semáforo
 	MOV R4, #1 ; Usaremos R4 para fazer nosso delay
 	ACALL DELAY
 
 ;Verde
-	MOV P1, #00100001B
 	JNB P1.3, CONTINUA2
 
 ;Valida btn pressionado
 	ACALL BTN_PRESS
 	CONTINUA2:
+	MOV P1, #00100001B
 	MOV R4, #1
 	ACALL DELAY
 
-;Amarelo
-	MOV P1, #00101010B
 	JNB P1.3, CONTINUA3
+;Amarelo
 
 ;Valida btn pressionado
 	ACALL BTN_PRESS
-	CONTINUA3:	
+	CONTINUA3:
+	MOV P1, #00100010B	
 	MOV R4, #1
 	ACALL DELAY
 	JMP INICIO
